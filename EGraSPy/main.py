ab69a3fed674aff4ae4d36058d6b7195610ec42d
Jeudy <jeudyx@gmail.com>
2014-11-05 16:52:09 +0100

    Small changes in parameters

--- EGraSPy/main.py
+++ EGraSPy/main.py
@@ -5,22 +5,23 @@ from utils import generate_sphere_position_distribution, \
 from physics import gravitational_acceleration
 from constants import AU, SUN_MASS, SECONDS_DAY
 
-N = 100
+N = 200
 STEPS = 50000
 DT = SECONDS_DAY
 
 particles_positions = generate_sphere_position_distribution(2*AU, N)
-masses = generate_mass_distribution(SUN_MASS, N, variability=0.5)
+masses = generate_mass_distribution(SUN_MASS/5000., N, variability=0.5)
 velocities = generate_velocities_distribution(N, -100.0, 100.0, no_z=True)
 accelerations = np.zeros((N, 3))
+itr = 0
 
 save_points(particles_positions, './data/positions_initial.csv')
 
 print "System initialized. Total mass: %s" % (masses.sum() / SUN_MASS)
 
 # Euler to start TODO use leap frog or Velet
-while STEPS > 0:
-#    start = time.time()
+while itr <= STEPS:
+    start = time.time()
     for i in range(0, N):
         p_i = particles_positions[i]
         m_i = masses[i]
@@ -35,9 +36,10 @@ while STEPS > 0:
             # Take advantage of Newton 3rd law
             accelerations[j] -= (acc * m_i) / m_j  # This may be unnecessary since all masses are equal
 
-#    end = time.time()
+    end = time.time()
 
-#    print "Forces calculation done in %s" % (end - start)
+    if itr == 0:
+        print "Forces calculation done in %s for first iteration..." % (end - start)
 
 #    start = time.time()
 
@@ -49,10 +51,10 @@ while STEPS > 0:
 
 #    print "Velocities and positions update done in %s" % (end - start)
 
-    STEPS -= 1
-    if STEPS % 500 == 0:
-        print "Voy por %s" % STEPS
+    itr += 1
+    if itr % 500 == 0:
+        print "Voy por %s" % itr
         print "Max velocity: %s -  Min velocity: %s" % (velocities.max(), velocities.min())
-        save_points(particles_positions, './data/positions_%s.csv' % STEPS)
+        save_points(particles_positions, './data/positions_%s.csv' % itr)
 
 print 'Done. Avg velocity in system %s' % (velocities.mean())
